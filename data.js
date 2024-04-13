e605c1f8b6e4d61ad91c8865bfc130f07a684b88
SindulaDevi <127840904+SindulaDevi@users.noreply.github.com>
2024-04-13 22:53:01 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,31 +2,31 @@ let movies = [
     {
         name: 'raya and the last dragon',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/slider 4.PNG'
+        image: 'slider 4.PNG'
     },
     {
         name: 'falcon and the winter soldier',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/slider 2.PNG'
+        image: 'slider 2.PNG'
     },
     {
         name: 'loki',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/lokiposter 1.PNG'
+        image: 'lokiposter 1.PNG'
     },
     {
         name: 'wanda vision',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/slider 3.PNG'
+        image: 'slider 3.PNG'
     },
     {
         name: 'raya and the last dragon',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/slider 4.PNG'
+        image: 'slider 4.PNG'
     },
     {
         name: 'luca',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/slider 5.PNG'
+        image: 'slider 5.PNG'
     }
 ]
